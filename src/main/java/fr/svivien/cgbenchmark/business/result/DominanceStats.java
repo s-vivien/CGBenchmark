b45febdec832c0ca5afe73e4fc06324691f1b87b
Agade09 <n.sillitoe@gmail.com>
2020-11-28 17:47:52 +0000

    Use exact error bars. https://sigmazone.com/binomial-confidence-intervals/.

--- src/main/java/fr/svivien/cgbenchmark/business/result/DominanceStats.java
+++ src/main/java/fr/svivien/cgbenchmark/business/result/DominanceStats.java
@@ -7,6 +7,7 @@ import java.text.NumberFormat;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import org.apache.commons.math3.distribution.BetaDistribution;
 
 /**
  * Holds statistics for a particular enemy, regardless of the matchup type (1v1, 1v2, 1v3)
@@ -18,7 +19,7 @@ public class DominanceStats {
         private double win, lose, draw;
     }
 
-    private static final String winrateOutputFormat = "%20s    GW=%-7s [%-6s,%-6s] [ W=%-7s  L=%-7s  D=%-7s ] [%s]%s";
+    private static final String winrateOutputFormat = "%20s    GW=%-7s [%-7s,%-7s] [W=%-7s L=%-7s D=%-7s] [%s]%s";
     private static final NumberFormat doubleFormatter = new DecimalFormat(Constants.DOUBLE_FORMAT);
 
     private final Map<Integer, String> nickPerAgentId = new HashMap<>();
@@ -81,37 +82,32 @@ public class DominanceStats {
         this.gameNumber++;
     }
 
-    private double[] calculateWinrateBounds(double winrate,double winrateSigma){
-        final double winrateUncertaintyDelta = 1.96*winrateSigma*100; //95% confidence interval
-        final double[] bounds = new double[]{Math.max(0,winrate-winrateUncertaintyDelta),Math.min(100,winrate+winrateUncertaintyDelta)};
+    private double[] calculateWinrateBounds(final double games,final double wins){
+        final double alpha=0.05;
+        BetaDistribution betaDist = new BetaDistribution(games-wins+1, wins+1e-7);
+        final double lower_bound=1-betaDist.inverseCumulativeProbability(1-alpha/2d);
+        betaDist = new BetaDistribution(games-wins+1e-7, wins+1);
+        final double upper_bound=1-betaDist.inverseCumulativeProbability(alpha/2d);
+        final double[] bounds = new double[]{100*lower_bound,100*upper_bound};
         return bounds;
     }
 
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
-        double overallSigma = 0;
-        double overallWinrateContributors = 0;
 
         for (Map.Entry<Integer, DominanceStat> entry : dominances.entrySet()) {
             if (entry.getKey() == -1) continue;
             DominanceStat dom = entry.getValue();
             int crashes = this.crashes.get(entry.getKey());
-            final double winrate = getMeanWinrate(entry.getKey());
-            final double winrateVariance = (winrate/100)*(1-(winrate/100))/dom.total; //Approximate variance sqrt(p*(1-p)). See variance of binomial distribution. Most accurate around 50% winrate, very inacurate at 0 and 100% where there is 0 error
-            final double winrateSigma = Math.sqrt(winrateVariance);
-            final double[] winrateBounds = dom.total>0?calculateWinrateBounds(winrate,winrateSigma):new double[]{0,100};
-            if(dom.total>0){
-                overallSigma += winrateVariance;
-                ++overallWinrateContributors;
-            }
+            final double[] winrateBounds = dom.total>0?calculateWinrateBounds(dom.total,dom.win+0.5*dom.draw):new double[]{0,100};
 
             builder.append(String.format(
                     winrateOutputFormat,
                     nickPerAgentId.get(entry.getKey()),
-                    doubleFormatter.format(winrate) + "%",
-                    doubleFormatter.format(winrateBounds[0]),
-                    doubleFormatter.format(winrateBounds[1]),
+                    doubleFormatter.format(getMeanWinrate(entry.getKey())) + "%",
+                    doubleFormatter.format(winrateBounds[0]) + "%",
+                    doubleFormatter.format(winrateBounds[1]) + "%",
                     doubleFormatter.format(getWinrate(entry.getKey())) + "%",
                     doubleFormatter.format(getLoserate(entry.getKey())) + "%",
                     doubleFormatter.format(getDrawrate(entry.getKey())) + "%",
@@ -119,17 +115,16 @@ public class DominanceStats {
                     crashes > 0 ? " [" + crashes + " crash" + (crashes > 1 ? "es" : "") + "]" : ""));
             builder.append(System.lineSeparator());
         }
-        overallSigma = Math.sqrt(overallSigma)/overallWinrateContributors; //Variance of the average https://stats.stackexchange.com/questions/168971/variance-of-an-average-of-random-variables
-        final double overallWinrate = getMeanWinrate(-1);
-        final double[] overallWinrateBounds = calculateWinrateBounds(overallWinrate,overallSigma);
+        DominanceStat totalStat = dominances.get(-1);
+        final double[] overallWinrateBounds = calculateWinrateBounds(totalStat.total,totalStat.win+0.5*totalStat.draw);// Can I really do it like this for the average winrate? Or must I somehow propagate the bounds from the individual winrates?
 
         int crashes = this.crashes.get(-1);
         builder.append(String.format(
                 winrateOutputFormat,
                 "-- EVERYONE --",
-                doubleFormatter.format(overallWinrate) + "%",
-                doubleFormatter.format(overallWinrateBounds[0]),
-                doubleFormatter.format(overallWinrateBounds[1]),
+                doubleFormatter.format(getMeanWinrate(-1)) + "%",
+                doubleFormatter.format(overallWinrateBounds[0]) + "%",
+                doubleFormatter.format(overallWinrateBounds[1]) + "%",
                 doubleFormatter.format(getWinrate(-1)) + "%",
                 doubleFormatter.format(getLoserate(-1)) + "%",
                 doubleFormatter.format(getDrawrate(-1)) + "%",
