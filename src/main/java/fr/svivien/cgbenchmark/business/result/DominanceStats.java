9b266fd434b3318adc188a9e8e29f7a4d0a92e6e
s-vivien <simon.vivien.1@gmail.com>
2020-12-08 20:30:49 +0100

    winrate bounds cleaning

--- src/main/java/fr/svivien/cgbenchmark/business/result/DominanceStats.java
+++ src/main/java/fr/svivien/cgbenchmark/business/result/DominanceStats.java
@@ -1,6 +1,7 @@
 package fr.svivien.cgbenchmark.business.result;
 
 import fr.svivien.cgbenchmark.utils.Constants;
+import org.apache.commons.math3.distribution.BetaDistribution;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
@@ -8,17 +9,10 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import org.apache.commons.math3.distribution.BetaDistribution;
-
 /**
  * Holds statistics for a particular enemy, regardless of the matchup type (1v1, 1v2, 1v3)
  */
 public class DominanceStats {
-
-    private class DominanceStat {
-        private int total;
-        private double win, lose, draw;
-    }
 
     private static final String winrateOutputFormat = "%20s    GW=%-7s [%-7s,%-7s] [W=%-7s L=%-7s D=%-7s] [%s]%s";
     private static final NumberFormat doubleFormatter = new DecimalFormat(Constants.DOUBLE_FORMAT);
@@ -31,6 +25,10 @@ public class DominanceStats {
     public DominanceStats() {
         dominances.put(-1, new DominanceStat());
         crashes.put(-1, 0);
+    }
+
+    double getGlobalMeanWinrate() {
+        return dominances.get(-1).getMeanWinrate();
     }
 
     void addEnemy(int agentId, String nick) {
@@ -56,41 +54,12 @@ public class DominanceStats {
         }
     }
 
-    private double getWinrate(int agentId) {
-        DominanceStat stat = dominances.get(agentId);
-        return stat.win != 0 ? (100.0 * stat.win / stat.total) : 0;
-    }
-
-    private double getLoserate(int agentId) {
-        DominanceStat stat = dominances.get(agentId);
-        return stat.lose != 0 ? (100.0 * stat.lose / stat.total) : 0;
-    }
-
-    private double getDrawrate(int agentId) {
-        DominanceStat stat = dominances.get(agentId);
-        return stat.draw != 0 ? (100.0 * stat.draw / stat.total) : 0;
-    }
-
-    double getMeanWinrate(int agentId) {
-        return getWinrate(agentId) + 0.5 * getDrawrate(agentId);
-    }
-
     void incrementCrash(int agentId) {
         this.crashes.put(agentId, this.crashes.get(agentId) + 1);
     }
 
     void incrementGameNumber() {
         this.gameNumber++;
-    }
-
-    private double[] calculateWinrateBounds(final double games, final double wins) {
-        final double alpha = 0.05;
-        BetaDistribution betaDist = new BetaDistribution(games - wins + 1, wins + 1e-7);
-        final double lower_bound = 1 - betaDist.inverseCumulativeProbability(1 - alpha / 2d);
-        betaDist = new BetaDistribution(games - wins + 1e-7, wins + 1);
-        final double upper_bound = 1 - betaDist.inverseCumulativeProbability(alpha / 2d);
-        final double[] bounds = new double[]{100 * lower_bound, 100 * upper_bound};
-        return bounds;
     }
 
     @Override
@@ -101,37 +70,68 @@ public class DominanceStats {
             if (entry.getKey() == -1) continue;
             DominanceStat dom = entry.getValue();
             int crashes = this.crashes.get(entry.getKey());
-            final double[] winrateBounds = dom.total > 0 ? calculateWinrateBounds(dom.total, dom.win + 0.5 * dom.draw) : new double[]{0, 100};
+            double[] winrateBounds = dom.getWinrateBounds();
 
             builder.append(String.format(
                     winrateOutputFormat,
                     nickPerAgentId.get(entry.getKey()),
-                    doubleFormatter.format(getMeanWinrate(entry.getKey())) + "%",
+                    doubleFormatter.format(dom.getMeanWinrate()) + "%",
                     doubleFormatter.format(winrateBounds[0]) + "%",
                     doubleFormatter.format(winrateBounds[1]) + "%",
-                    doubleFormatter.format(getWinrate(entry.getKey())) + "%",
-                    doubleFormatter.format(getLoserate(entry.getKey())) + "%",
-                    doubleFormatter.format(getDrawrate(entry.getKey())) + "%",
+                    doubleFormatter.format(dom.getWinrate()) + "%",
+                    doubleFormatter.format(dom.getLoserate()) + "%",
+                    doubleFormatter.format(dom.getDrawrate()) + "%",
                     dom.total,
                     crashes > 0 ? " [" + crashes + " crash" + (crashes > 1 ? "es" : "") + "]" : ""));
             builder.append(System.lineSeparator());
         }
         DominanceStat totalStat = dominances.get(-1);
-        final double[] overallWinrateBounds = calculateWinrateBounds(totalStat.total, totalStat.win + 0.5 * totalStat.draw);// Can I really do it like this for the average winrate? Or must I somehow propagate the bounds from the individual winrates?
-
+        double[] overallWinrateBounds = totalStat.getWinrateBounds();
         int crashes = this.crashes.get(-1);
         builder.append(String.format(
                 winrateOutputFormat,
                 "-- EVERYONE --",
-                doubleFormatter.format(getMeanWinrate(-1)) + "%",
+                doubleFormatter.format(totalStat.getMeanWinrate()) + "%",
                 doubleFormatter.format(overallWinrateBounds[0]) + "%",
                 doubleFormatter.format(overallWinrateBounds[1]) + "%",
-                doubleFormatter.format(getWinrate(-1)) + "%",
-                doubleFormatter.format(getLoserate(-1)) + "%",
-                doubleFormatter.format(getDrawrate(-1)) + "%",
+                doubleFormatter.format(totalStat.getWinrate()) + "%",
+                doubleFormatter.format(totalStat.getLoserate()) + "%",
+                doubleFormatter.format(totalStat.getDrawrate()) + "%",
                 gameNumber + " games",
                 crashes > 0 ? " [ME : " + crashes + " crash" + (crashes > 1 ? "es" : "") + "]" : ""));
 
         return builder.toString();
+    }
+
+    private class DominanceStat {
+        private int total;
+        private double win, lose, draw;
+
+        double getWinrate() {
+            return win != 0 ? (100.0 * win / total) : 0;
+        }
+
+        double getLoserate() {
+            return lose != 0 ? (100.0 * lose / total) : 0;
+        }
+
+        double getDrawrate() {
+            return draw != 0 ? (100.0 * draw / total) : 0;
+        }
+
+        double getMeanWinrate() {
+            return this.getWinrate() + 0.5 * this.getDrawrate();
+        }
+
+        double[] getWinrateBounds() {
+            if (total <= 0) return new double[]{0, 100};
+            double meanWins = win + 0.5 * draw;
+            double alpha = 0.05;
+            BetaDistribution betaDist = new BetaDistribution(total - meanWins + 1, meanWins + 1e-7);
+            double lower_bound = 1 - betaDist.inverseCumulativeProbability(1 - alpha / 2d);
+            betaDist = new BetaDistribution(total - meanWins + 1e-7, meanWins + 1);
+            double upper_bound = 1 - betaDist.inverseCumulativeProbability(alpha / 2d);
+            return new double[]{100 * lower_bound, 100 * upper_bound};
+        }
     }
 }
